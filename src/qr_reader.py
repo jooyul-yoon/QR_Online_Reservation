2d0ef5afaba4c108ee6b3b416d824a91dcd0bc12
Jooyul Yoon <juyeolyoon@gmail.com>
2021-07-26 12:04:14 -0700

    refactored

--- src/qr_reader.py
+++ src/qr_reader.py
@@ -8,38 +8,18 @@ import time
 import sys
 import openpyxl
 
-# def add_label():
-#     global DELAY
-#     global countLabel
-#     if DELAY == 0:
-#         body_frame.destroy()
-#     if DELAY == -1:
-#         return
-#     if countLabel:
-#         countLabel.pack_forget()
-#     # print("Foward:  " + str(DELAY) + "m")
-#     DELAY = DELAY - 1
-#     start_frame.after(1000, add_label)
-def show_label(self, event=None):
-		self.label.lift(self.frame)
-
-def hide_label(self, event=None):
-		self.label.lower(self.frame)
-def refresh(self):
-    self.destroy()
-    self.__init__()
-
 def clear_frame(frame):
 	for widgets in frame.winfo_children():
 		widgets.pack_forget()
 
 
-# Ask which service
-service_num = 1 # int(input("Which Service(1-3): "))
-# while service_num <= 0 | service_num >= 4:
-#     print("Enter between 1 - 3.")
-#     service_num = int(input("Which service(1-3): "))
+## Choose Service ##
+service_num = int(input("Which Service(1-3): "))
+while service_num <= 0 | service_num >= 4:
+    print("Enter between 1 - 3.")
+    service_num = int(input("Which service(1-3): "))
 
+## set time ##
 now = datetime.now()
 month_list = {1:"Jan ", 2:"Feb ", 3:"Mar ", 4:"Apr ", 5:"May ", 6:"Jun ",
               7:"Jul ", 8:"Aug ", 9:"Sep ", 10:"Oct ", 11:"Nov ", 12:"Dec "}
@@ -49,27 +29,34 @@ month = now.month
 date = str(now.day)
 year = str(now.year)
 
-this_service = month_list[month] + date + ", " + year + service_list[service_num]  # Nov 13, 2020 at 10:30 AM
+this_service = month_list[month] + date + ", " + year + service_list[service_num]
 print("Today's Service: " + this_service)
 
-loc  = "data/report-2020-11-10T1105.xlsx"  # EventBrite List
-loc2 = ("data/11082020_list.xlsx")           # pastor/volunteer
+## database path ##
+# EventBrite List #
+loc  = "data/report-2020-11-10T1105.xlsx"
+# Pastor / Volunteers
+loc2 = ("data/11082020_list.xlsx")
 
-# input file prep
+# input file prep #
 wb = openpyxl.load_workbook(loc)
 sheet = wb.active
-#
-# format checking
+
+# pastor/volunteer list loading
+wb = openpyxl.load_workbook(loc2)
+pSheet = wb.active
+
+# format checking #
 if sheet['J1'].value != "Barcode #":
     print("ERROR: Barcode # is not on excel file")
     quit()
 
-# output file prep
+# output file prep #
 logfile = ("log/" + now.strftime("%Y_%m_%d__%H_%M_%S")+".log")
 f = open(logfile, "x", encoding='utf-8')  # create file for writing
 f.write(this_service + "\n")
 
-# read-up ticket and makes dictionary
+# read-up ticket and makes dictionary #
 attendee_list = {
         "barcode#": ["name", "email", "phone", "checkin"]
 }
@@ -78,6 +65,7 @@ entered_person = {
         "barcode#": "Time"
 }
 
+## insert data to database ##
 for i in range(2, sheet.max_row + 1):
     first_name  = sheet.cell(i, 5).value
     last_name   = sheet.cell(i, 6).value
@@ -88,11 +76,7 @@ for i in range(2, sheet.max_row + 1):
     phone = sheet.cell(i,13).value
     attendee_list[barcode_num] = [name, email, phone, checkIn]
 
-# pastor/volunteer list loading
-wb = openpyxl.load_workbook(loc2)
-pSheet = wb.active
-
-
+# Pastors / Volunteers
 for i in range(2, pSheet.max_row + 1):
     pName = pSheet.cell(i, 2).value
     barcode_num = pSheet.cell(i, 1).value
@@ -106,7 +90,7 @@ for i in range(2, pSheet.max_row + 1):
 
 # print(attendee_list)
 
-# GUIs
+## GUIs ##
 ROOT = Tk()
 ROOT.title("EPC Sunday Service")
 ROOT.tk_setPalette(background='white')
@@ -114,6 +98,7 @@ ROOT.geometry('1680x800')
 frame = Frame(ROOT);
 frame.pack();
 
+## images ##
 wel_img1 = ImageTk.PhotoImage(Image.open("img/welcome1.png"))
 wel_img2 = ImageTk.PhotoImage(Image.open("img/welcome2.png"))
 wel_img3 = ImageTk.PhotoImage(Image.open("img/welcome3.png"))
@@ -129,27 +114,29 @@ if service_num == 2:
     header = Image.open("img/welcome2.png")
 if service_num == 3:
     header = Image.open("img/welcome3.png")
-header = Image.open("img/welcome3.png")
 
 photo = ImageTk.PhotoImage(header)
 headerLabel = Label(image=photo)
 headerLabel.image = photo
 headerLabel.place(x=64, y=52)
 
-# Body frame
+## Body frame ##
 body_frame = Frame(ROOT,width=1000,height=600, padx=20, pady=20,bg='white')
 body_frame.pack()
 body_frame.place(x=550, y=221)
 
+## image labels ##
 qrLabel = Label(body_frame, image=qr_img)
 confirmedLabel = Label(body_frame, image=confirmed_img)
 notLabel = Label(body_frame, image=notOnTime_img)
 unregLabel = Label(body_frame, image=notreg_img)
 redeemedLabel = Label(body_frame, image=redeemed_img)
 
-nameText = Text(body_frame, width=32, height=1, font=("Arial", 19))
-descText = Text(body_frame, width=32, height=1, font=("Arial", 19))
+## Text labels ##
+nameText = Text(body_frame, pady=30, width=32, height=1, font=("Arial", 30))
+descText = Text(body_frame, width=32, height=1, font=("Arial", 30))
 
+## Main function ##
 exit_loop = False
 qrLabel.pack()
 while exit_loop == False:
@@ -223,5 +210,4 @@ while exit_loop == False:
 			sys.stdout.flush()
 			# GUI
 			clear_frame(body_frame)
-			unregLabel.pack()
-	
+			unregLabel.pack()	
